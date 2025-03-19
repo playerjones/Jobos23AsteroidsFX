cf446c830daecaa78b629c6ebc1d45528d836bfa
playerjones <jonasbossen99@hotmail.com>
2025-03-19 20:38:44 +0100

    POM FIXED LETS GO
    Fixed the poms to finally work

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module Core {
     requires Common;
     requires javafx.graphics;
     requires javafx.controls;
-    exports dk.sdu.cbse.main;
     opens dk.sdu.cbse.main to javafx.graphics;
     uses dk.sdu.cbse.common.services.IGamePluginService;
     uses dk.sdu.cbse.common.services.IEntityProcessingService;
