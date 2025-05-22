7c16ac1caf31c0f851d1deb9f393a8d7e3415db7
playerjones <jonasbossen99@hotmail.com>
2025-05-22 22:08:25 +0200

    common Asteroids added

---
+++ CommonAsteroids/src/main/java/dk/sdu/cbse/common/asteroids/IAsteroidSplitter.java
@@ -0,0 +1,9 @@
+package dk.sdu.cbse.common.asteroids;
+
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.data.World;
+
+
+public interface IAsteroidSplitter {
+    void createSplitAsteroid(Entity e, World w);
+}
