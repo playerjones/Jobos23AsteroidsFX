00f5eef0f92b276844f829ea1cfbe11c8a388191
playerjones <jonasbossen99@hotmail.com>
2025-05-23 08:24:25 +0200

    update

---
+++ Collision/src/main/java/dk/sdu/cbse/collision/CollisionSystem.java
@@ -0,0 +1,21 @@
+package dk.sdu.cbse.collision;
+
+import dk.sdu.cbse.common.data.Entity;
+import dk.sdu.cbse.common.data.GameData;
+import dk.sdu.cbse.common.data.World;
+import dk.sdu.cbse.common.services.IPostEntityProcessingService;
+import dk.sdu.cbse.common.bullet.Bullet;
+import dk.sdu.cbse.common.asteroids.Asteroid;
+
+public class CollisionSystem implements IPostEntityProcessingService {
+    @Override
+    public void process(GameData gameData, World world) {
+        for (Entity bullet : world.getEntities(Bullet.class)) {
+            for (Entity asteroid : world.getEntities(Asteroid.class)) {
+                CollisionDetector detector = new CollisionDetector();
+                detector.checkCollision(bullet, asteroid, world);
+            }
+        }
+        // You can add more collision checks (e.g., player vs asteroid) here
+    }
+}
--- Collision/src/main/java/module-info.java
+++ Collision/src/main/java/module-info.java
@@ -1,4 +1,9 @@
+import dk.sdu.cbse.common.services.IPostEntityProcessingService;
+
 module Collision {
     requires Common;
+    requires CommonBullet;
+    requires CommonAsteroids;
     exports dk.sdu.cbse.collision;
+    provides IPostEntityProcessingService with dk.sdu.cbse.collision.CollisionSystem;
 }
