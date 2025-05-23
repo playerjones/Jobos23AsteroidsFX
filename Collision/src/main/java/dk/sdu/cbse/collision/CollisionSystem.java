db389d299eaebcac34300c4f3eaf48b4fb7457c1
playerjones <jonasbossen99@hotmail.com>
2025-05-23 08:38:39 +0200

    fixed collision for player and asteroid

--- Collision/src/main/java/dk/sdu/cbse/collision/CollisionSystem.java
+++ Collision/src/main/java/dk/sdu/cbse/collision/CollisionSystem.java
@@ -1,11 +1,11 @@
 package dk.sdu.cbse.collision;
 
+import dk.sdu.cbse.common.asteroids.Asteroid;
+import dk.sdu.cbse.common.bullet.Bullet;
 import dk.sdu.cbse.common.data.Entity;
 import dk.sdu.cbse.common.data.GameData;
 import dk.sdu.cbse.common.data.World;
 import dk.sdu.cbse.common.services.IPostEntityProcessingService;
-import dk.sdu.cbse.common.bullet.Bullet;
-import dk.sdu.cbse.common.asteroids.Asteroid;
 
 public class CollisionSystem implements IPostEntityProcessingService {
 
@@ -20,10 +20,23 @@ public class CollisionSystem implements IPostEntityProcessingService {
             }
         }
 
-        // Player vs Asteroid
+        // Player vs Asteroid (module-agnostic: look for Player class by name)
+        for (Entity entity : world.getEntities()) {
+            if (entity.getClass().getSimpleName().equals("Player")) {
+                for (Entity asteroid : world.getEntities(Asteroid.class)) {
+                    detector.checkCollision(entity, asteroid, world);
+                }
+            }
+        }
 
-
-        // Asteroid vs Asteroid (avoid self-collision)
-
+        // Asteroid vs Asteroid (avoid self-collision and duplicate checks)
+        java.util.List<Entity> asteroids = world.getEntities(Asteroid.class);
+        for (int i = 0; i < asteroids.size(); i++) {
+            Entity a1 = asteroids.get(i);
+            for (int j = i + 1; j < asteroids.size(); j++) {
+                Entity a2 = asteroids.get(j);
+                detector.checkCollision(a1, a2, world);
+            }
+        }
     }
 }
