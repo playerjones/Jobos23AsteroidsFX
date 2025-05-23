81621d5c3e858314d6bbd4d459d72f5c52349f3e
playerjones <jonasbossen99@hotmail.com>
2025-05-23 08:35:42 +0200

    Update CollisionSystem.java

--- Collision/src/main/java/dk/sdu/cbse/collision/CollisionSystem.java
+++ Collision/src/main/java/dk/sdu/cbse/collision/CollisionSystem.java
@@ -6,7 +6,6 @@ import dk.sdu.cbse.common.data.World;
 import dk.sdu.cbse.common.services.IPostEntityProcessingService;
 import dk.sdu.cbse.common.bullet.Bullet;
 import dk.sdu.cbse.common.asteroids.Asteroid;
-import dk.sdu.cbse.common.player.Player;
 
 public class CollisionSystem implements IPostEntityProcessingService {
 
@@ -22,18 +21,9 @@ public class CollisionSystem implements IPostEntityProcessingService {
         }
 
         // Player vs Asteroid
-        for (Entity player : world.getEntities(Player.class)) {
-            for (Entity asteroid : world.getEntities(Asteroid.class)) {
-                detector.checkCollision(player, asteroid, world);
-            }
-        }
+
 
         // Asteroid vs Asteroid (avoid self-collision)
-        var asteroids = world.getEntities(Asteroid.class);
-        for (int i = 0; i < asteroids.size(); i++) {
-            for (int j = i + 1; j < asteroids.size(); j++) {
-                detector.checkCollision(asteroids.get(i), asteroids.get(j), world);
-            }
-        }
+
     }
 }
